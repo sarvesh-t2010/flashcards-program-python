462b936bdfa75aac698da1d73f51a59fe458314c
Sarvesh Thiagarajan <sarvesh.t2010@gmail.com>
2025-10-14 22:28:17 +0000

    linear regression model

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import random
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import time
+
 final_flashcards = []
 repeat_flashcards = []
-repeat_count = 0
+repeat_count = 1
+start_time = 0
+end_time = 0
+total_time = []
+session_start_time = 0
+session_end_time = 0
 saved_flashcards = []
+unique_list = []
+repeat_list = []
+
 
 """
 To do:
@@ -13,6 +26,29 @@ documentation
 comments
 formatting
 """
+
+
+def model(x, y):
+    x_coord = np.array(x).reshape(-1, 1)
+    y_coord = np.array(y)
+
+    model = LinearRegression()
+    model.fit(x_coord, y_coord)
+
+    y_pred = model.predict(x_coord)
+
+    plt.scatter(x_coord, y_coord, label = "real data", marker = 'o')
+    plt.plot(x_coord, y_pred, color = 'red', label = "Predicted time to memorise")
+    plt.title("Predicted time to study")
+    plt.xlabel("Times flashcards were repeated")
+    plt.ylabel("Time taken to complete")
+    plt.legend()
+    plt.grid(True)
+    plt.show(block=False)
+    plt.pause(5)
+    plt.clf()
+
+
 
 while True:
     try:
@@ -48,8 +84,9 @@ for i in range(len(seperated_flashcards)):
 
 
 random.shuffle(final_flashcards)
-
+session_start_time = time.time()
 while True:
+    start_time = time.time()
     for j in range(2):
         for i in range(len(final_flashcards)):
             print("")
@@ -57,26 +94,49 @@ while True:
             while True:
                 known_var = str(input("Do you know the answer to this? (Yes or No): "))
                 known_var = known_var.strip()
-                if known_var == "yes" or known_var == "Yes":
+                known_var = known_var.lower()
+                if known_var == "yes":
                     print("Great work!")
                     break
-                elif known_var == "no" or known_var == "No":
+                elif known_var == "no":
                     repeat_flashcards.append(final_flashcards[i])
                     print("That's okay! Practice makes perfect!")
                     break
                 else:
                     print("Thats an invalid input! Say Yes or No!")
         
-        print("\nHere is the answer to '" + final_flashcards[i][0] + "':")
-        print(final_flashcards[i][1])
-
+            print("\nHere is the answer to '" + final_flashcards[i][0] + "':")
+            print(final_flashcards[i][1])
+        
+        random.shuffle(final_flashcards)
+        
+    
     final_flashcards = []
     final_flashcards = repeat_flashcards
     repeat_flashcards = []
+    unique_list = []
+    for i in final_flashcards:
+        if i not in unique_list:
+            unique_list.append(i)
+    
+    final_flashcards = unique_list
+
     if len(final_flashcards) < 1:
         print("Nice work! You memorised all your flashcards!")
         break
     else:
-        print("\nYou have a few more flashcards left to memorise! Repeating cards left to memorise...")
+        print("\nYou have " + str(len(final_flashcards))+ " more flashcards left to memorise! Repeating cards left to memorise...")
+        
+        repeat_count = repeat_count + 1
+        repeat_list.append(repeat_count)
+        end_time = time.time()
+        total_time.append(round(end_time-start_time, 2))
+        if repeat_count > 2:
+            model(repeat_list, total_time)
         continue
+
+session_end_time = time.time()
+
+session_total_time = session_end_time - session_start_time
+print("\nYou spent " + str(round(session_total_time/60, 2)) + " minutes studying!")
 
